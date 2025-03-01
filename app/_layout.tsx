76fbd1ebaf3220d12cbf9380b17e4a7844535c51
Tim H <timothyhyland@BatBook-Pro.local.tcghome.pro>
2025-03-01 14:56:16 -0500

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,36 @@
+import { Stack, useRouter, useSegments, useNavigation } from 'expo-router';
+import { AuthProvider, useAuth } from '../context/AuthContext';
+import { useEffect } from 'react';
+
+const StackLayout = () => {
+	const { authState } = useAuth();
+	const segments = useSegments();
+	const router = useRouter();
+
+	useEffect(() => {
+		const inAuthGroup = segments[0] === '(protected)';
+    console.log(authState, inAuthGroup)
+		if (!authState?.authenticated && inAuthGroup) {    
+      router.replace("/")
+		} else if (authState?.authenticated === true) {
+			router.replace('./(protected)');
+		}
+	}, [authState]);
+
+	return (
+		<Stack>
+			<Stack.Screen name="index" options={{ headerShown: false }} />
+			<Stack.Screen name="(protected)" options={{ headerShown: false }} />
+		</Stack>
+	);
+};
+
+const RootLayoutNav = () => {
+	return (
+		<AuthProvider>
+			<StackLayout />
+		</AuthProvider>
+	);
+};
+
+export default RootLayoutNav;
